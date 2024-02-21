48a1020c0e1a3ec16b1f4efd6aa77f9c9d7687b7
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2024-02-21 18:13:43 +0500

    Add Javascript questions

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
 console.log('Algoritm ');
+
+/////////////////////////////////
+// 1-savol:
+// Stringni kirish sifatida qabul qiladigan va Stringni teskari holatga qaytaradigan funksiya yozing.
+
+// O'tilgan mavzular: String manipulyatsiyasi, for loop.
+// Kirish namunasi: "salom"
+// Namuna chiqishi: "olleh"
+
+function reverseString(str) {
+  //Shu yerga yozing 👇
+}
+
+console.log(reverseString('Algoritm'));
+
+/////////////////////////////////
+// 2-savol:
+// 1 dan 20 gacha bo'lgan raqamlarni chop etuvchi funksiyani yozing.
+// Ammo uch soniga qoldiqsiz bo'linadigan son o'rniga "Fizz" ni, besh soniga qoldiqsiz bo'linadigan son uchun esa
+// "Buzz" ni chop eting. Uch va beshga soniga qoldiqsiz bo'linadigan raqamlar uchun "FizzBuzz" ni chop eting.
+
+function fizzBuzz() {
+  //Shu yerga yozing 👇
+}
+
+console.log(fizzBuzz());
+
+////////////////////////////////
+// 3-savol:
+// Massivda(Array) eng tez-tez uchraydigan elementni topish funksiyasini yozing.
+
+// O'tilgan mavzular: massivlar, for loop, if-else, ob'ektni manipulyatsiya qilish.
+// Kirish namunasi: [1, 3, 2, 3, 2, 3, 2, 1, 2, 2]
+// Namuna chiqishi: 2
+
+function mostFrequent(arr) {
+  //Shu yerga yozing 👇
+}
